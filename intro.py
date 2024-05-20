73cce11472530849be99db8ddf2e348d7b891b67
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-05-20 13:03:17 +0300

    introduction to python

---
+++ intro.py
@@ -0,0 +1,38 @@
+# this a comment
+# loosely dynamically typed lang. --> detect datatype in the runtime
+
+name = 'ahmed'  # string
+track = 'bi'  # string
+grade = 100  # int
+temp = 34.4  # float
+happy = True  # bool
+
+# any line in python --> must be written from the beginning of line
+name = 'noha'
+
+# no braces
+x = 10
+if x == 10:
+    print("found")  # indentation --> tab
+
+message = 'hello iti'
+
+message2 = "hello python"
+
+bio = ('My name is Noha '
+       'I works at ITI'
+       'I lives in cairo')
+
+print(bio)
+
+bio2 = '''My name is Noha
+I works at ITI
+I lives in cairo'''
+print(bio2)
+
+# this a comment
+
+"""this string can be treated as a comment"""
+print(type(name))
+
+""" type conversion"""
